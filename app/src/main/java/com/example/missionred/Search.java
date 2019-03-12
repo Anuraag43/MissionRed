92a69e4f1016ff0d083327ec1f121358b7b20318
ANURAG ALLADI <S533567@nwmissouri.edu>
2019-03-11 23:36:57 -0500

    updated Search.java

---
+++ app/src/main/java/com/example/missionred/Search.java
@@ -0,0 +1,21 @@
+package com.example.missionred;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class Search extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search);
+    }
+
+    public void call(View view)
+    {
+        Intent contact = new Intent(Intent.ACTION_DIAL);
+        startActivity(contact);
+    }
+}
