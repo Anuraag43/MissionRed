91ae025412e9f340f31e3ace6e09bce21e70c750
Anuraag Naidu Sanakkayala <anuraagsan7@gmail.com>
2019-03-19 00:11:17 -0500

    added search functionality

--- app/src/main/java/com/example/missionred/Search.java
+++ app/src/main/java/com/example/missionred/Search.java
@@ -9,18 +9,15 @@ public class Search extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        
         super.onCreate(savedInstanceState);
+        
         setContentView(R.layout.activity_search);
     }
 
-
-
-
-
-
     public void call(View view)
     {
-        Intent contact = new Intent(Intent.ACTION_DIAL);
-        startActivity(contact);
+        Intent contacts = new Intent(Intent.ACTION_DIAL);
+        startActivity(contacts);
     }
 }
