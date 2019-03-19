c004ddf20d3c8a41439a4de37f3952b91bfdf9a9
harishvavilala <s534119@nwmissouri.edu>
2019-03-18 23:00:07 -0500

    I Updated search part of the file

--- app/src/main/java/com/example/missionred/Search.java
+++ app/src/main/java/com/example/missionred/Search.java
@@ -9,14 +9,14 @@ public class Search extends AppCompatActivity {
 
 
 
-
-
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
     }
+
+
+
 
     public void call(View view)
     {
